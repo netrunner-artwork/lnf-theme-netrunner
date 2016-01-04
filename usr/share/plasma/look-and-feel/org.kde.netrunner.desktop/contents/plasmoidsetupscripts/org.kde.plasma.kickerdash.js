a1719c78ad4ad8a4e7ccf38f548ba47ddefde21e
AJSlye <jameskittsmiller@gmail.com>
2016-01-04 18:59:01 -0500

    Add kicker based defaults

---
+++ usr/share/plasma/look-and-feel/org.kde.netrunner.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickerdash.js
@@ -0,0 +1,3 @@
+applet.currentConfigGroup = ["General"]
+applet.writeConfig("favoriteApps", ["systemsettings.desktop", "org.kde.ksysguard.desktop", "octopi.desktop", "vokoscreen.desktop"])
+applet.writeConfig("limitDepth", true)
