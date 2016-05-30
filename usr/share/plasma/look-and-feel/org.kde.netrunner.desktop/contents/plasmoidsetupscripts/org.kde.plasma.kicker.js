7fbe62221c034c32b42f0cce5c89c44bf3525706
AJSlye <jameskittsmiller@gmail.com>
2016-05-30 12:16:43 -0400

    Updates to menu and EITM creation

--- usr/share/plasma/look-and-feel/org.kde.netrunner.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kicker.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kicker.js
@@ -1,3 +1,8 @@
+applet.currentConfigGroup = ["Shortcuts"]
+applet.writeConfig("global", "Alt+F1")
+
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("favoriteApps", ["systemsettings.desktop", "org.kde.ksysguard.desktop", "octopi.desktop", "vokoscreen.desktop"])
 applet.writeConfig("limitDepth", true)
+
+applet.reloadConfig();
--- usr/share/plasma/look-and-feel/org.kde.netrunner.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
@@ -1,2 +1,7 @@
+applet.currentConfigGroup = ["Shortcuts"]
+applet.writeConfig("global", "Alt+F1")
+
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("favorites", ["systemsettings.desktop", "org.kde.ksysguard.desktop", "octopi.desktop", "vokoscreen.desktop"])
+
+applet.reloadConfig();
